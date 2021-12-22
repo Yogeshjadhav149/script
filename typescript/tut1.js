52ae52fb988ea8d9904f3580470993d99689c908
yogesh.jadhav149 <yogesh.jadhav149@gmail.com>
2021-12-22 10:33:38 +0530

    tutorial

---
+++ typescript/tut1.js
@@ -0,0 +1,4 @@
+var first_name = "yogesh";
+var last_name = "jadhav";
+var marks = 500;
+console.log(first_name + " " + last_name + " you scored " + marks);
---
+++ typescript/tut1.ts
@@ -0,0 +1,5 @@
+let first_name="yogesh";
+let last_name="jadhav";
+let marks=500;
+
+console.log(first_name +" "+ last_name +" you scored "+ marks);
